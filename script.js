c165d80cddc8b76d8dfe19117519571272e865be
Anna <anysofronova@yandex.ru>
2020-10-21 12:19:41 +0300

    Взаимодействие: alert, prompt, confirm
    Изучила взаимодействие: alert, prompt, confirm

---
+++ script.js
@@ -0,0 +1,8 @@
+// Переменные
+// let name = 'Джон';
+// let admin = name;
+// alert(admin);
+
+// Взаимодействие: alert, prompt, confirm
+// let name = prompt('Введите ваше Имя', '');
+// alert(`Привет, ${name}`);
